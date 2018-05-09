7c5a1a38f2aa845fd28d0e90190b35ad44830b96
David Chiu <davidwchiu@gmail.com>
2018-05-09 21:13:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  attr_acecssor name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all.each do |dog|
+      puts dog
+    end 
+
+end
+    
